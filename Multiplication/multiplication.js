8239d6dded4b8aa53a90e9b503cc6c3e3c9f3477
azadnn <76432992+azadnn@users.noreply.github.com>
2025-09-27 04:55:28 -0700

    Initial site

---
+++ Multiplication/multiplication.js
@@ -0,0 +1,86 @@
+function isDuplicateQuestion(newQuestion, questionBank) {
+    const [newFirst, newSecond, newAnswer] = newQuestion
+    for (let queNum = 0; queNum < questionBank.length; ++queNum) {
+        const [oldFirst, oldSecond, oldAnswer] = questionBank[queNum]
+        if (oldFirst==newFirst && oldSecond==newSecond) return true;
+    }
+    return false;
+}
+
+function allQuestions(lowestNum, highestNum) {
+    currentTable = [];
+    let firstNumber
+    let secondNumber
+    let currentList;
+    let rangeForRandom = highestNum - lowestNum +1
+    while (currentTable.length < totalNumQuestions) {
+        firstNumber = Math.floor(Math.random() * rangeForRandom + lowestNum)
+        secondNumber = Math.floor(Math.random() * rangeForRandom + lowestNum)
+        currentList = [firstNumber, secondNumber, firstNumber * secondNumber]
+        if (!(isDuplicateQuestion(currentList, currentTable))) {currentTable.push(currentList)}
+    }
+    return currentTable
+}
+
+function pass() {/*Does nothing*/}
+
+function checkSolution() {
+    const expectedAnswer = questionsList[currentQuestion-1][missingIndex]
+    if (theTextBox.value == `${expectedAnswer}`) {
+        theRightOrWrong.innerHTML = 'Right!'
+        gottenRight++;
+    } else {
+        theRightOrWrong.innerHTML = `Wrong! The answer is ${expectedAnswer}`
+    }
+
+    wrongQuestions = totalNumQuestions-gottenRight;
+    theButton.disabled = true;
+    setTimeout(() => {
+        console.log("After 3 seconds");
+        theRightOrWrong.innerHTML = ''
+        theTextBox.value = ''
+        theButton.disabled = false;
+            if (currentQuestion >= totalNumQuestions) {
+        document.body.innerHTML = `Questions gotten right: ${gottenRight}
+    <br>Questions gotten wrong: ${wrongQuestions}
+    <br>Total score: ${gottenRight}/${totalNumQuestions}`
+    } else {
+        missingIndex = generateNewQuestion()
+    }
+
+    }, 3000);
+
+}
+
+function generateNewQuestion() {
+    const que = questionsList[currentQuestion]
+    const missing = "__"
+    const randomNum = Math.floor(Math.random() * que.length)
+    const firstNumber = randomNum==0? missing: que[0]
+    const secondNumber = randomNum==1? missing: que[1]
+    const answer = randomNum==2? missing: que[2]
+
+    console.log(currentQuestion);
+    currentQuestion += 1;
+    theQuestionNumber.textContent = `Question ${currentQuestion}`
+    document.getElementById("question").innerHTML = `${firstNumber} x ${secondNumber} = ${answer}`
+    return randomNum
+}
+
+let currentQuestion = 0;
+let gottenRight = 0;
+let totalNumQuestions = 30;
+
+let lowestNum = 2;
+let highestNum = 12;
+
+let questionsList = allQuestions(lowestNum, highestNum)
+
+const theButton = document.getElementById("solve")
+const theTextBox = document.getElementById("textbox")
+const theRightOrWrong = document.getElementById("right-or-wrong")
+const theQuestionNumber = document.getElementById("question-number")
+
+let missingIndex=generateNewQuestion()
+
+                                
